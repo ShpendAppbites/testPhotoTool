16e47091dc3ab5daf13400ec7474fc7e1888ba0b
Shpend Tahiri <shpend.tahiri15@gmail.com>
2023-10-04 11:26:17 +0200

    Add PhotoCaptureView and CameraView

--- Classes/testPhotoTool.swift
+++ Classes/testPhotoTool.swift
@@ -2,16 +2,37 @@
 //  testPhotoTool.swift
 //  Pods-testPhotoTool_Example
 //
-//  Created by appbites on 10/3/23.
+//  Created by appbites on 10/4/23.
 //
 
 import SwiftUI
+import AVFoundation
 
-@main
-struct testPhotoTool: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+public struct PhotoCaptureView: View {
+    @State private var isCameraActive = false
+    @State private var capturedImage: UIImage?
+
+    public var body: some View {
+        VStack {
+            Button("Capture Photo") {
+                isCameraActive = true
+            }
+            .sheet(isPresented: $isCameraActive) {
+                CameraView(capturedImage: $capturedImage, isCameraActive: $isCameraActive)
+            }
+
+            if let image = capturedImage {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
         }
+    }
+}
+
+struct PhotoCaptureView_Previews: PreviewProvider {
+    static var previews: some View {
+        PhotoCaptureView()
     }
 }
