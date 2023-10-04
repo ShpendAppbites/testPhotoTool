c3b40b0d5477f153bdf511715239d90dee765f1c
Shpend Tahiri <shpend.tahiri15@gmail.com>
2023-10-04 13:52:17 +0200

    add the camera func

--- Classes/CameraView.swift
+++ Classes/CameraView.swift
@@ -1,76 +1,29 @@
-//
-//  CameraView.swift
-//  Pods-testPhotoTool_Example
-//
-//  Created by appbites on 10/3/23.
-//
-
-
-//import SwiftUI
-//import UIKit
-//
-//struct CameraView: UIViewControllerRepresentable {
-//    @Binding var image: UIImage?
-//    @Environment(\.presentationMode) private var presentationMode
-//
-//    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-//        var parent: CameraView
-//
-//        init(_ parent: CameraView) {
-//            self.parent = parent
-//        }
-//
-//        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-//            if let originalImage = info[.originalImage] as? UIImage {
-//                parent.image = originalImage
-//            }
-//            parent.presentationMode.wrappedValue.dismiss()
-//        }
-//
-//        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-//            parent.presentationMode.wrappedValue.dismiss()
-//        }
-//    }
-//
-//    func makeCoordinator() -> Coordinator {
-//        Coordinator(self)
-//    }
-//
-//    func makeUIViewController(context: Context) -> UIImagePickerController {
-//        let picker = UIImagePickerController()
-//        picker.delegate = context.coordinator
-//        picker.sourceType = .camera // Set to .photoLibrary to open the photo library instead
-//        // picker.mediaTypes = ["public.image"]
-//        return picker
-//    }
-//
-//    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
-//        // Nothing to do here
 //    }
 //}
 import SwiftUI
+import UIKit
 import AVFoundation
 
-struct CameraView: View {
+public struct CameraView: View {
     @Binding var capturedImage: UIImage?
     @Binding var isCameraActive: Bool
+    @State private var imagePickerPresented = true
+    @State private var imagePickerSourceType: UIImagePickerController.SourceType = .camera
 
-    var body: some View {
+    public var body: some View {
         VStack {
-            Text("Camera View")
-
-            Button("Capture") {
-                // Implement the camera capture logic here
-            }
-            .padding()
-
-            Button("Dismiss") {
-                isCameraActive = false
-            }
-            .padding()
         }
         .onAppear {
             // Initialize the camera here, if needed
         }
+        .sheet(isPresented: $imagePickerPresented) {
+            ImagePickerView(sourceType: imagePickerSourceType) { image in
+                // Handle the captured image
+                capturedImage = image
+                isCameraActive = false
+            }
+        }
     }
 }
+
+
--- Classes/testPhotoTool.swift
+++ Classes/testPhotoTool.swift
@@ -1,24 +1,27 @@
-//
-//  testPhotoTool.swift
-//  Pods-testPhotoTool_Example
-//
-//  Created by appbites on 10/4/23.
-//
-
 import SwiftUI
 import AVFoundation
 
 public struct PhotoCaptureView: View {
-    @State private var isCameraActive = false
-    @State private var capturedImage: UIImage?
+        @Binding var capturedImage: UIImage?
+        @Binding var isCameraActive: Bool
+    @State private var imagePickerSourceType: UIImagePickerController.SourceType = .camera
 
+    public init(capturedImage: Binding<UIImage?>, isCameraActive: Binding<Bool>) {
+            self._capturedImage = capturedImage
+            self._isCameraActive = isCameraActive
+        }
+    
     public var body: some View {
         VStack {
             Button("Capture Photo") {
                 isCameraActive = true
             }
             .sheet(isPresented: $isCameraActive) {
-                CameraView(capturedImage: $capturedImage, isCameraActive: $isCameraActive)
+                ImagePickerView(sourceType: imagePickerSourceType) { image in
+                    // Handle the captured image
+                    capturedImage = image
+                    isCameraActive = false
+                }
             }
 
             if let image = capturedImage {
@@ -31,8 +34,54 @@ public struct PhotoCaptureView: View {
     }
 }
 
+
+
 struct PhotoCaptureView_Previews: PreviewProvider {
+    @State private static var previewImage: UIImage?
+    @State private static var isPreviewCameraActive = false
+
     static var previews: some View {
-        PhotoCaptureView()
+        PhotoCaptureView(capturedImage: $previewImage, isCameraActive: $isPreviewCameraActive)
+    }
+}
+
+struct ImagePickerView: UIViewControllerRepresentable {
+    typealias UIViewControllerType = UIImagePickerController
+    typealias Callback = (UIImage?) -> Void
+
+    let sourceType: UIImagePickerController.SourceType
+    let callback: Callback
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = sourceType
+        imagePicker.delegate = context.coordinator
+        return imagePicker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePickerView
+
+        init(_ parent: ImagePickerView) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let selectedImage = info[.originalImage] as? UIImage {
+                parent.callback(selectedImage)
+            } else {
+                parent.callback(nil)
+            }
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.callback(nil)
+        }
     }
 }
